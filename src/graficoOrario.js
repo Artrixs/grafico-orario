7f964aa4a1aa49fce9d214029aededf22ee57a3a
Artrixs <misino.arturo@gmail.com>
2020-10-30 15:15:22 +0100

    Refactor the way the GraficoOrario draws the lines
    Now the App class is responsible for the trains while
    graficoOrario manages the drawing

--- src/graficoOrario.js
+++ src/graficoOrario.js
@@ -1,6 +1,6 @@
 
 class GraficoOrario {
-    constructor(canvas, stations) {
+    constructor(canvas, stations, app) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.height = canvas.height;
@@ -8,6 +8,7 @@ class GraficoOrario {
         this.padding = 20;
         this.stationListWidth = 200 //Thewidth of the station area
         this.hoursHeight = 50 // Height of the x axis
+        this.app = app;
 
         this.stations = stations;
         for( const s in stations){
@@ -22,7 +23,6 @@ class GraficoOrario {
 
         this.timeStart = new Date();
         this.timeEnd = new Date(this.timeStart.getTime() + 1000 * 60 * 60 * 4);
-        this.trains = {};
 
         window.addEventListener('load', () => {
             this.resize();
@@ -31,7 +31,7 @@ class GraficoOrario {
             this.resize();
         });
         canvas.addEventListener("wheel", (event) => {
-            var change = Math.sign(event.deltaY) * 1000 * 60;
+            const change = Math.sign(event.deltaY) * 1000 * 60;
             if ( event.ctrlKey ) {
                 this.changeTimeZoom( change );
             } else {
@@ -42,70 +42,79 @@ class GraficoOrario {
         })
     }
 
-    addTrain(number, category){
-        this.trains[number] = {
-            type: category,
-            stops: []
+    // Returns the border of the drawable area of the canvas
+    getBorders() {
+        return {
+            topY: this.padding,
+            bottomY: this.height - (this.padding + this.hoursHeight),
+            leftX: this.padding + this.stationListWidth,
+            rightX: this.width - this.padding,
         }
     }
 
-    addTrainStop(train, station, time) {
-        this.trains[train].stops.push({station:station, time:time});
-    }
+    drawLine( points, color, selected ) {
+        const leftOfTimeFrame = (time) => { return time < this.timeStart };
+        const rightOfTimeFrame = (time) => { return time > this.timeEnd };
+        const bothOnTheSameSide = (time1, time2) => {
+            return  (leftOfTimeFrame(time1) && leftOfTimeFrame(time2)) ||
+                    (rightOfTimeFrame(time1) && rightOfTimeFrame(time2));
+        }
+        const insideTimeFrame = (time) => { return !leftOfTimeFrame(time) && !rightOfTimeFrame(time) };
 
-    drawTrain(category, stops){
-        //TODO: change color based on category
-        //FIXME: if both one time point is before and the next one is after we are not drawing anything!
         this.ctx.save();
-        this.ctx.lineWidth = 2;
+        this.ctx.lineWidth = selected ? 4 : 2;
+        this.ctx.strokeStyle = color;
         this.ctx.beginPath();
-        const insideTimeFrame = (time) => { return time >= this.timeStart && time <= this.timeEnd };
-        for( let i = 0; i < stops.length - 1; i++) {
-            const stop = stops[i];
-            const nextStop = stops[i+1];
-            const x = this.getXfromTime( stop.time );
-            const y = this.getYFromKM( this.stations[stop.station].km);
-            const nextX = this.getXfromTime( nextStop.time );
-            const nextY = this.getYFromKM( this.stations[nextStop.station].km);
-            if ( insideTimeFrame(stop.time) && insideTimeFrame(nextStop.time)){
-                //both stops are inside the time frame
-                if( i == 0 ) { //This is also the first one we have so we have to move the cursor first
-                    this.ctx.moveTo(x,y);
-                }
-                this.ctx.lineTo(nextX, nextY);
-                continue
-            }
-            if( insideTimeFrame(stop.time)) {
-                //The next one is outside ( after this iteration we can stop)
-                if( i == 0 ) {// if it'st the first we have to first move the cursor
-                    this.ctx.moveTo(x,y);
-                }
-                const slope = (nextY-y)/(nextX-x);
-                const newY = y + slope*( this.width - this.padding-x);
-                this.ctx.lineTo(this.width - this.padding, newY);
-                break;
-            }
-            if( insideTimeFrame(nextStop.time)) {
-                // The nextStop is inside while this is not
-                const slope = (nextY-y)/(nextX-x);
-                const newY = y + slope * (this.padding + this.stationListWidth-x);
-                this.ctx.moveTo(this.padding + this.stationListWidth, newY);
-                this.ctx.lineTo(nextX, nextY);
+
+        for( let i = 0; i < points.length -1; i++) {
+            if( bothOnTheSameSide( points[i].time, points[i+1].time ) ){
                 continue;
             }
+
+            if ( i == 0 ) {
+                this.ctx.moveTo(this.getXfromTime( points[i].time) , this.getYFromStation( points[i].station));
+            }
+
+            if ( insideTimeFrame(points[i].time) && insideTimeFrame(points[i+1].time) ) {
+                this.ctx.lineTo( this.getXfromTime( points[i+1].time ), this.getYFromStation( points[i+1].station ));
+            } else {
+                this.drawCroppedLine( points[i], points[i+1] );
+            }
         }
+
         this.ctx.stroke();
         this.ctx.restore();
     }
 
+    drawCroppedLine(start, end) {
+        const { leftX, rightX } = this.getBorders();
+        const startX = this.getXfromTime( start.time );
+        const startY = this.getYFromStation( start.station );
+        const endX   = this.getXfromTime( end.time );
+        const endY   = this.getYFromStation( end.station );
+        const slope  = (endY - startY)/(endX - startX);
+
+        if( start.time < this.timeStart ) {
+            this.ctx.moveTo(leftX, startY + slope * (leftX - startX) ) //Set start on the Y axis
+        }
+
+        if ( end.time > this.timeEnd ) {
+            this.ctx.lineTo(rightX, startY + slope * (rightX - startX) );
+        } else {
+            this.ctx.lineTo( endX, endY);
+        }
+    }
+
     drawUI() {
-        var ctx = this.ctx;
+        const {topY, bottomY, leftX, rightX} = this.getBorders();
+        const ctx = this.ctx;
+
         ctx.save();
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.moveTo(this.padding + this.stationListWidth, this.padding);
-        ctx.lineTo(this.padding + this.stationListWidth, this.height - (this.padding + this.hoursHeight));
-        ctx.lineTo(this.width - this.padding, this.height - (this.padding + this.hoursHeight));
+        ctx.moveTo(leftX, topY);
+        ctx.lineTo(leftX, bottomY);
+        ctx.lineTo(rightX, bottomY);
         ctx.stroke();
         ctx.restore();
 
@@ -114,27 +123,28 @@ class GraficoOrario {
     }
 
     drawTimescale() {
+        const {leftX, rightX, topY, bottomY } = this.getBorders();
         this.drawTimeOnTimescale(this.timeStart);
         this.drawTimeOnTimescale(this.timeEnd);
-        const startX = this.getXfromTime(this.timeStart);
-        const endX = this.getXfromTime(this.timeEnd);
 
         this.ctx.save();
-        this.ctx.beginPath();
         this.ctx.setLineDash([10,8]);
         this.ctx.strokeStyle = "lightgray";
-        this.ctx.moveTo(endX, this.padding);
-        this.ctx.lineTo(endX, this.height - (this.padding + this.hoursHeight) + 3);
+        this.ctx.beginPath();
 
         for(const e of this.getTimesForTimescale()){
             const x = this.getXfromTime(e);
-            if ( x - startX < 15 || endX - x < 15){
+            if ( x - leftX < 15 || rightX - x < 15){
                 continue;
             }
-            this.ctx.moveTo(x, this.padding);
-            this.ctx.lineTo(x, this.height - ( this.padding + this.hoursHeight) + 3);
+            this.ctx.moveTo(x, topY);
+            this.ctx.lineTo(x, bottomY + 3);
             this.drawTimeOnTimescale(e);
         }
+
+        this.ctx.moveTo(rightX, topY);
+        this.ctx.lineTo(rightX, bottomY + 3);
+
         this.ctx.stroke();
         this.ctx.restore();
     }
@@ -147,6 +157,7 @@ class GraficoOrario {
         const startTime = this.timeStart.getTime();
         const endTime = this.timeEnd.getTime();
         const timeDelta = endTime - startTime;
+
         let timeInterval = 0;
         if ( timeDelta > 1000 * 60 * 60 * 4) { //4hours
             timeInterval = 1000 * 60 * 30; //30mins
@@ -157,9 +168,10 @@ class GraficoOrario {
         } else {
             timeInterval = 1000 * 60 * 5;
         }
-        const first = (Math.trunc( startTime / timeInterval) + 1)* timeInterval;
+
+        const first = (Math.trunc( startTime / timeInterval) + 1) * timeInterval;
         
-        var result = [];
+        let result = [];
         for( let time = first; time < endTime; time += timeInterval){
             result.push(new Date(time));
         }
@@ -178,9 +190,11 @@ class GraficoOrario {
             text += "0";
         }
         text += minutes;
+
+        const { bottomY } = this.getBorders();
         this.ctx.save();
         this.ctx.font = '16px Roboto';
-        this.drawVerticalText(text, this.getXfromTime(time), this.height - (this.padding+this.hoursHeight)+10)
+        this.drawVerticalText(text, this.getXfromTime(time), bottomY +10)
         this.ctx.restore();
     }
 
@@ -201,22 +215,30 @@ class GraficoOrario {
     }
 
     getXfromTime(time) {
+        const { leftX, rightX } = this.getBorders();
         var startToEndDelta = this.timeEnd.getTime() - this.timeStart.getTime();
         var startToTimeDelta = time.getTime() - this.timeStart.getTime();
-        var startToEndPixels = this.width - ( this.padding * 2 + this.stationListWidth );
+        var startToEndPixels = rightX - leftX;
         var startToTimePixels = Math.floor( ( startToTimeDelta / startToEndDelta ) * startToEndPixels );
         return this.padding + this.stationListWidth + startToTimePixels;
     }
 
     getYFromKM(km) {
+        const { topY, bottomY } = this.getBorders();
         const totalDeltaKM = this.maxKM - this.minKM;
         const partialDeltaKM = km - this.minKM;
-        const totalPixels = this.height - ( this.padding * 2 + this.hoursHeight );
+        const totalPixels = bottomY - topY;
         const partialPixels = Math.floor( (partialDeltaKM / totalDeltaKM) * totalPixels );
         return (this.height - ( this.padding + this.hoursHeight ) ) - partialPixels;
     }
 
+    getYFromStation(station) {
+        return this.getYFromKM( this.stations[station].km );
+    }
+
     drawStationscale() {
+        const { leftX, rightX } = this.getBorders();
+
         this.ctx.save();
         const baseFont = ' 14px Roboto';
         this.strokeStyle = 'lightgray';
@@ -225,11 +247,11 @@ class GraficoOrario {
             this.ctx.font = this.getFontFromStationType(s.type, baseFont);
             const Xoffset = this.ctx.measureText(name).width +5;
             const y = this.getYFromKM(s.km);
-            this.drawHorizontalText(name, this.padding + this.stationListWidth - Xoffset, y);
+            this.drawHorizontalText(name, leftX - Xoffset, y);
             
             this.ctx.beginPath();
-            this.ctx.moveTo(this.padding + this.stationListWidth - 2, y);
-            this.ctx.lineTo(this.width - this.padding, y);
+            this.ctx.moveTo(leftX - 2, y);
+            this.ctx.lineTo(rightX, y);
             this.ctx.stroke();
         }
         this.ctx.restore();
@@ -258,9 +280,10 @@ class GraficoOrario {
     update() {
         this.clearCanvas();
         this.drawUI();
-        
-        for( let t in this.trains ) {
-            this.drawTrain(this.trains[t].type, this.trains[t].stops);
+
+        for( const e of this.app.getTrainLines() ){
+            const {points, color, selected} = e;
+            this.drawLine(points, color, selected);
         }
     }
 
@@ -276,9 +299,9 @@ class GraficoOrario {
     }
 
     resize() {
-        var table = document.querySelector('table');
-        var header = document.querySelector('header');
-        var footer = document.querySelector('footer');
+        const table = document.querySelector('table');
+        const header = document.querySelector('header');
+        const footer = document.querySelector('footer');
     
         this.canvas.width = window.innerWidth - table.offsetWidth;
         this.canvas.height = window.innerHeight - (header.offsetHeight + footer.offsetHeight);
